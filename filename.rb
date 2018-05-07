4e2ef4a1f8fb32f9535b3356b8d6ad3180a26b77
Richard Rosa Serrano <richardrosaserrano@gmail.com>
2018-05-07 11:37:53 -0600

    Richard is still making changes

--- filename.rb
+++ filename.rb
@@ -1,11 +1,12 @@
 class FakeClass
-<<<<<<< HEAD
+<<<<<<< HEAD <<<< FOOT
   def initialize
     @stuff = stuff
   end
 =======
   ##FAKE NEWS!!!!!!!!!
->>>>>>> b0120cf78ad5815939bc516e24b8acc4013fbba7
+>>>>>>> b0120cf78ad5815939bc516e24b8acc4013fbba7 <<< WHAT THEEE FUUUUUUUU
 end
 
 ##RICHARD IS ADDING THINGSSSSSSSS
+#Richard IS keeping in changing the status quo!
